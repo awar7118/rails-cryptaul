39511819282045a59f1e53db6fb31f5330abe57a
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-01 18:12:09 +0000

    put fetch api in connect method and created method to begin to select divs and rename their innertext with data from the API

--- app/javascript/controllers/getcryptos_controller.js
+++ app/javascript/controllers/getcryptos_controller.js
@@ -1,17 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
-// APIURL link is from and can be altereed at https://www.coingecko.com/en/api/documentation
-apiurl =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%201y";
+// APIURL link is from and can be altered at https://www.coingecko.com/en/api/documentation
 
 export default class extends Controller {
   //select targets
   connect() {
     console.log("The 'get cryptos' controller is now loaded!");
-  }
-
-  fetchCryptos(event) {
-    fetch(apiurl)
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%201y"
+    )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setCryptoInfo(data));
   }
 }
+const setCryptoInfo = (data) => {
+  data.forEach((crypto) => {
+    const cryptoPrice = document.querySelector("#cryptoPrice");
+    const cryptoName = document.querySelector("#cryptoName");
+    const cryptoTicker = document.querySelector("#cryptoTicker");
+    const cryptoMarketCap = document.querySelector("#cryptoMarketCap");
+
+    cryptoPrice.innerText = crypto.current_price;
+    cryptoName.innerText = crypto.id;
+    cryptoTicker.innerText = crypto.btc;
+    cryptoMarketCap.innerText = crypto.location;
+  });
+};
