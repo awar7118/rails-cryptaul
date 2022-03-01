1b1309cd4b45a84c53743bdf21d8d92238402d96
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-01 17:06:49 +0000

    added methods to stimulus controllers

--- app/javascript/controllers/getcryptos_controller.js
+++ app/javascript/controllers/getcryptos_controller.js
@@ -1,16 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
+// APIURL link is from and can be altereed at https://www.coingecko.com/en/api/documentation
+apiurl =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%201y";
 
 export default class extends Controller {
   //select targets
-  static targets = ["checkbox"];
   connect() {
     console.log("The 'get cryptos' controller is now loaded!");
   }
 
   fetchCryptos(event) {
-    this.checkboxTargets.forEach((checkbox) => {
-      checkbox.checked = event.currentTarget.checked;
-      console.log(checkbox);
-    });
+    fetch(apiurl)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 }
