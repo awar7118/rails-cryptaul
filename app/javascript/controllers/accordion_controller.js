aed3c9d16d452f245637da211397710385148ad7
Tara Culpin <taraculpin@gmail.com>
2022-03-02 23:39:05 +0000

    sticky buy button, about and stats accordions

---
+++ app/javascript/controllers/accordion_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["content"]
+
+  // connect() {
+  //   this.hiTarget.innerHTML = 'Hello, dropdown!'
+  //   console.log("accordion controller is connected")
+  
+  expand() {
+    this.contentTarget.classList.toggle("collapse")
+  }
+
+}
