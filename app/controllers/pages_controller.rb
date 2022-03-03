d20656a147432ee2ac141a3a9ddd48e19bf5dc2b
Jeremiah Harriott <harriott_47@hotmail.com>
2022-03-03 15:59:03 +0000

    added to dashboard created cards and adding color to cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def my_dashboard
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cryptos, only: :index
+  resources :cryptos, only: [:index, :show]
+  resources :holdings, only: :index
+  get "my_dashboard", to: "pages#my_dashboard"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
