32a14f4b230b953ff3102ad3c6e7da517a97fcf2
Tara Culpin <taraculpin@gmail.com>
2022-03-03 16:49:31 +0000

    added basic chart to the show page and started on making it work on desktop

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,4 +1,6 @@
 class CryptosController < ApplicationController
+  require 'json'
+
   before_action :find_crypto, only: :show
   def index
     @cryptos = Crypto.all
@@ -11,5 +13,9 @@ class CryptosController < ApplicationController
 
   def find_crypto
     @crypto = Crypto.find(params[:id])
+    @price_data = @crypto.histories.map do |price|
+      [price.date, price.price]
+    end
+    p @price_data
   end
 end
