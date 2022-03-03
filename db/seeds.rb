647cdd4cf9a897ad712d6b8dcd8490c0f133e9e2
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-03 12:38:50 +0000

    sorted index page to list price and name

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,21 @@ require 'faker'
 require 'open-uri'
 Crypto.destroy_all
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&     per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%201y"
-
-cryptos = JSON.read("./db/jsondata/crypto.json")
+# Opening dataset of top 100 cryptos from the crypto.json file
+cryptos = JSON.parse(File.read("./db/jsondata/crypto.json"))
 # p cryptos
 
-
+# Iterating of each of the 100 cryptos and creating a crypto from it
 cryptos.each do |c|
   crypto = Crypto.create!(
-  name: c["name"],
-  abbreviation: c["symbol"]
+    name: c["name"],
+    abbreviation: c["symbol"]
   )
   puts "created coin #{crypto.name}"
-
-  prices = JSON.read("./db/jsondata/#{crypto.name}.json")
-  prices["prices"].each do |price|
+  # Opening pircing data for each crypto and get the last 365 days of data to populate our histories table in our database with
+  prices = JSON.parse(File.read("./db/jsondata/#{crypto.name}.json"))
+  prices["prices"].last(365).each do |price|
+    # Need to remove .last(365) to save all historical data. We are only saving time now for development.
     History.create!(
       price: price[1],
       date: Time.at(price[0]),
