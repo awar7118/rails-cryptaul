98ce03b84a50745010afc992dbce47fd35e3deee
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-03-04 17:13:50 +0000

    fix pish to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,17 @@
 
 require 'faker'
 require 'open-uri'
+
+# puts "Deleting all holdings"
+# Holding.destroy_all
+# puts "Completed deletion of all holdings"
+
 puts "Deleting all cryptos"
 Crypto.destroy_all
 puts "Completed deletion of all cryptos"
+
+# user = User.new(first_name: 'Sol', last_name: 'Karim', email: 'test10@test.com', password: 'hello123', simulation_date: Time.at(1614816000))
+
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&     per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2030d%2C%201y"
 # Opening dataset of top 100 cryptos from the crypto.json file
 cryptos = JSON.parse(File.read("./db/jsondata/crypto.json"))
