371bfb45aef72735241d34ee5b78719647d18d48
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-03-04 14:23:24 +0000

    fixed migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_03_04_125749) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
