44d5d47b2390aec53f0d1301d7f5e52b66a19af9
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-03-04 14:02:22 +0000

    add logic for user to simulate date and get correct crypto prices for that date. this also allows user to show the value of their wallet in the simulated date, compared to the value at the time they purchased the crypto

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get 'my_dashboard', to: 'holdings#index'
   get 'articles', to: 'pages#articles'
+  get 'my_dashboard/:id', to: 'holdings#advance_date', as: 'change_simulation'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
