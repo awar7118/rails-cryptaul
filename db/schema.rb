1a6b4f86f6ab3ac5d56a41bba5fa0daea77cb8c2
Tara Culpin <taraculpin@gmail.com>
2022-03-04 08:19:38 +0000

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_03_03_235807) do
+ActiveRecord::Schema.define(version: 2022_03_03_202926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_235807) do
     t.string "abbreviation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "histories", force: :cascade do |t|
