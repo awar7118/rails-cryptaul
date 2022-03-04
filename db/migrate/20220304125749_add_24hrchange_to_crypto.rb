6f846c264362cc12bc5e27aa6c9cc2847d0cd159
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-03-04 13:05:33 +0000

    added red or green font logic to index page 24hr change

--- db/migrate/20220304125749_add_24hrchange_to_crypto.rb
+++ db/migrate/20220304125749_add_24hrchange_to_crypto.rb
@@ -1,5 +1,5 @@
 class Add24hrchangeToCrypto < ActiveRecord::Migration[6.1]
   def change
-    add_column :cryptos, :24hourchange, :float
+    add_column :cryptos, :previousdaypercentagechange, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_202926) do
+ActiveRecord::Schema.define(version: 2022_03_04_125749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_202926) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.float "previousdaypercentagechange"
   end
 
   create_table "histories", force: :cascade do |t|
