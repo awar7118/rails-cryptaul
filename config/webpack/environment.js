e5070800cb09bddbb6089f6b84a9e0494f4283d4
Jeremiah Harriott <harriott_47@hotmail.com>
2022-03-08 15:32:59 +0000

    changes made to blockchain wording in jorgon busta

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const customConfig = require('./custom');
+const webpack = require('webpack');
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+environment.config.merge(customConfig);
+
 module.exports = environment
